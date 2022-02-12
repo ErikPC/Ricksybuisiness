957504f884de7bd43347186a0013e0ae1794144c
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-12 21:03:09 +0100

    feat: crear interfaz GuestDispatcher

---
+++ ricksybuisiness/src/main/java/edu/poniperro/ricksybuisiness/services/GuestDispatcher.java
@@ -0,0 +1,7 @@
+package edu.poniperro.ricksybuisiness.services;
+
+import edu.poniperro.ricksybuisiness.creditCard.CreditCard;
+
+public interface GuestDispatcher {
+    public void dispatch(CreditCard trajeta);
+}
